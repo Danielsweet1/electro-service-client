d2813b023a87c6d994e4b47d0e059b2aa72b52a1
Daniel Sweet <sweetpalma9@gmail.com>
2022-11-08 11:30:48 +0700

    data added

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -9,14 +9,19 @@ const Banner = () => {
           <div>
             <img src={banner} className=" rounded-lg h-96 shadow-2xl" />
           </div>
-          <div >
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <div className="text-center md:text-start">
+            <h1 className="text-5xl font-bold">
+              <span className="text-4xl">FAST AND RELIABLE</span> <br />
+              <span className="text-red-500 font-bolder">
+                ELECTRICAL SERVICES
+              </span>
+            </h1>
             <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              Looking for the best electrical service at home near you?
+              Electro has trusted & skilled electrician.
+              Satisfaction guaranteed.
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <button className="btn bg-red-500 text-white hover:bg-red-700 border-none">Book Now</button>
           </div>
         </div>
       </div>
