7c80fc50fe86ffbf872f170b59f9fcbb6f691cb9
Daniel Sweet <sweetpalma9@gmail.com>
2022-11-08 12:29:20 +0700

    added service card

---
+++ src/components/Shared/ServiceCard/ServiceCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ServiceCard = ({ service }) => {
+  const { serviceName, price, rating, img, description } = service;
+  return (
+    <div>
+      <div className="card card-compact rounded-xl w-96 bg-base-100 border shadow-xl">
+        <figure>
+          <img className="h-60 w-80 rounded-xl mt-5" src={img} alt="Services" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title font-bold ">{serviceName}</h2>
+            <div className="flex justify-between items-center">
+            <p className="text-xl font-bold">Price: <span className="text-red-500">${price}</span></p>
+            <p className="font-bold">Rating: <span className="text-orange-500">{rating}</span></p>
+            </div>
+          <p>{description.slice(0, 100) + '....'}</p>
+          <div className="">
+            <button className="btn bg-red-500 hover:bg-red-700 border-none my-3">Check Details</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCard;
